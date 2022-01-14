bec4c96ee69d2291c8f47bcb5d9f14e66cf093cd
Brent <brent.schriver@gmail.com>
2022-01-13 21:45:56 -0600

    Updating time-series notes and exercises.

--- acquire.py
+++ acquire.py
@@ -9,6 +9,7 @@ import seaborn as sns
 import sklearn.preprocessing
 from scipy.stats import pearsonr, spearmanr
 from scipy import stats
+
 # modeling methods
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression, LassoLars, TweedieRegressor
@@ -18,6 +19,9 @@ from sklearn.metrics import explained_variance_score
 import sklearn.linear_model
 import sklearn.feature_selection
 import sklearn.preprocessing
+
+# import modules for api requests
+import requests
 
 # import imputing function
 from sklearn.impute import SimpleImputer
@@ -187,42 +191,94 @@ def get_zillow_data():
 
 #******************************************** Acquire Data from https://python.zgulde.net**************************************************#
 
-def get_all(endpoint):
-    """ Read all records on all pages """
-    
-    if endpoint not in ["sales", "items", "stores"]:
-        return "Not available from this API. Check the documentation"
-    
-    host = "https://python.zgulde.net/"
-    api = "api/v1/"
+def new_retail_data(base_url='https://python.zgulde.net'):
+    '''
+    This function acquires new retail data, returns three dataframes, and saves those dataframes to .csv files.
+    '''
+    # Acquiring items data
+    response = requests.get('https://python.zgulde.net/api/v1/items')
+    data = response.json()
+    df = pd.DataFrame(data['payload']['items'])
+    while data['payload']['next_page'] != None:
+        response = requests.get(base_url + data['payload']['next_page'])
+        data = response.json()
+        df = pd.concat([df, pd.DataFrame(data['payload']['items'])]).reset_index(drop=True)
+    items_df = df.copy()
+    print("Items data acquired...")
 
-    url = host + api + endpoint
+    # Acquiring stores data
+    response = requests.get('https://python.zgulde.net/api/v1/stores')
+    data = response.json()
+    df = pd.DataFrame(data['payload']['stores'])
+    stores_df = df.copy()
+    print("Stores data acquired...")
 
-    response = requests.get(url)
+    # Acquiring sales data
+    response = requests.get('https://python.zgulde.net/api/v1/sales')
+    data = response.json()
+    df = pd.DataFrame(data['payload']['sales'])
+    while data['payload']['next_page'] != None:
+        response = requests.get(base_url + data['payload']['next_page'])
+        data = response.json()
+        df = pd.concat([df, pd.DataFrame(data['payload']['sales'])]).reset_index(drop=True)
+    sales_df = df.copy()
+    print("Sales data acquired")
 
-    if response.ok:
-        payload = response.json()["payload"]
+    # Saving new data to .csv files
+    items_df.to_csv("items.csv", index=False)
+    stores_df.to_csv("stores.csv", index=False)
+    sales_df.to_csv("sales.csv", index=False)
+    print("Saving data to .csv files")
 
-        # endpoint should be "items", "sales", or "stores"
-        contents = payload[endpoint]
+    return items_df, stores_df, sales_df
 
-        # Make a dataframe of the contents
-        df = pd.DataFrame(contents)
+def prep_store_data():
+    '''
+    This function reads in retail data from the website if there are no csv files to pull from
+    '''
+    # Checks if .csv files are present. If any are missing, will acquire new data for all three datasets
+    if (os.path.isfile('items.csv') == False) or (os.path.isfile('sales.csv') == False) or (os.path.isfile('stores.csv') == False) or (os.path.isfile('all_stores.csv') == False):
+        print("Data is not cached. Acquiring new data...")
+        items_df, stores_df, sales_df = new_retail_data()
+        
+        combined_df = sales_df.merge(items_df, how='left', left_on='item', right_on='item_id').drop(columns=['item'])
+        combined_df = combined_df.merge(stores_df, how='left', left_on='store', right_on='store_id').drop(columns=['store'])
 
-        next_page = payload["next_page"]
+        combined_df.to_csv("all_stores.csv", index=False)
+        print('Saving all stores data to csv.')    
+    else:
+        print("Data is cached. Reading from .csv files")
+        items_df = pd.read_csv('items.csv')
+        print("Items data acquired...")
+        stores_df = pd.read_csv('stores.csv')
+        print("Stores data acquired...")
+        sales_df = pd.read_csv('sales.csv')
+        print("Sales data acquired...")
+        combined_df = pd.read_csv('all_stores.csv')
+        print("All stores data cached. Reading from .csv files")
+    return combined_df
 
-        while next_page:
-            # Append the next_page url piece
-            url = host + next_page
-            response = requests.get(url)
-
-            payload = response.json()["payload"]
-
-            next_page = payload["next_page"]    
-            contents = payload[endpoint]
-
-            df = pd.concat([df, pd.DataFrame(contents)])
-
-            df = df.reset_index(drop=True)
-
+def get_store_data():
+    df = prep_store_data()
+    # Reassign the sale_date column to be a datetime type
+    df.sale_date = pd.to_datetime(df.sale_date)
+    # Sort rows by the date and then set the index as that date
+    df = df.set_index("sale_date").sort_index()
     return df
+
+def new_power_data():
+    opsd = pd.read_csv("https://raw.githubusercontent.com/jenfly/opsd/master/opsd_germany_daily.csv")
+    opsd = opsd.fillna(0)
+    print("Saving data to .csv file")
+    opsd.to_csv('opsd_germany_daily_data.csv', index=False)
+    return opsd
+
+def get_power_data():
+    if os.path.isfile('opsd_germany_daily_data.csv') == False:
+        print("Data is not cached. Acquiring new power data.")
+        opsd = new_power_data()
+    else:
+        print("Data is cached. Reading data from .csv file.")
+        opsd = pd.read_csv('opsd_germany_daily_data.csv')
+    print("Acquisition complete")
+    return opsd
